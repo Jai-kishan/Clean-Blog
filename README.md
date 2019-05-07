a75b435759e37f5fc23e63aa9455b7adce6a4599
Jai-kishan <jaikishan17@navgurukul.org>
2019-05-07 22:41:16 +0530

    update

--- README.md
+++ README.md
@@ -15,6 +15,30 @@ jai@jai:~/Downloads$ sudo su
 [sudo] password for jai:  
 root@jai:/home/jai/Downloads# ./xampp-linux-x64-7.1.28-0-installer.run
 ```
+after that if you want to run this software so :- 
+```
+jai@jai:~$ cd /opt/lampp/
+jai@jai:/opt/lampp$ sudo ./manager-linux-x64.run
+```
+
+https://www.wikihow.com/Install-XAMPP-on-Linux
+
+
+https://askubuntu.com/questions/892461/lampp-mysql-cant-create-err-file-and-doesnt-have-uninstall-file
+https://askubuntu.com/questions/145110/running-xampp-does-not-work
+
+
+### delete database
+```
+cd /opt/lampp/var/mysql;
+sudo su;
+rm -rf Navgurukul;
+```
+
+### open databse on terminal 
+```
+root@jai:~# mysql -h 127.0.0.1 -P 3306 -u root -p Navgurukul
+```
 
 ### Flask-SQLAlchemy
 If you want to read more about Flask-SQLalchemy so you can simply visit on this link :- 
@@ -49,3 +73,9 @@ AFTER
 ```
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/Navgurukul'
 ```
+
+
+### Flask-Mail
+this is a link of official documentation for flask mail :- 
+https://pythonhosted.org/Flask-Mail/
+
--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import json
+
+
+from flask_mail import Mail
+
+
+with open("config.json", 'r') as file:
+    params = json.load(file)['params']
+
+local_server=True
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/Navgurukul'
+app.config.update(
+    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_PORT='465',
+    MAIL_USE_SSL = True,
+    MAIL_USERNAME= params['gmail_id'],
+    MAIL_PASSWORD=params['gmail_password']
+    )
+
+mail = Mail(app)
+
+if (local_server):
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+
 db = SQLAlchemy(app)
 
 # hmnne class ka naam Cotactsislie rakha hai bcz our table name is "contacts"
-
 class Contacts(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
@@ -16,23 +39,34 @@ class Contacts(db.Model):
     date = db.Column(db.String(12), nullable=True)
     email = db.Column(db.String(20), nullable=True)
 
+class Posts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(30), nullable=False)
+    title = db.Column(db.String(80), nullable=False)
+    slug = db.Column(db.String(25),nullable=False)
+    content = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
 
 
 @app.route("/")
-def home():
-    return render_template('index.html')
 
-@app.route("/post")
-def post():
-    return render_template('post.html')
+def home():
+
+    return render_template('index.html', params = params)
+
+
+@app.route("/post/<string:post_slug>", methods=["GET"])
+def post_route(post_slug):
+    post = Posts.query.filter_by(slug = post_slug).first()
+    return render_template('post.html', params = params, post = post)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+
+    return render_template('about.html', params = params)
 
 @app.route("/contact", methods= ['GET','POST'])
 def contact():
-    # Add entry to the database
     if request.method == "POST":
         name = request.form.get('name')
         email = request.form.get('email')
@@ -42,7 +76,11 @@ def contact():
         entry = Contacts(name=name, phone_num=phone, msg=message, date = datetime.now(), email=email)
         db.session.add(entry)
         db.session.commit()
-
-    return render_template('contact.html')
+        mail.send_message(name + "share post for Jai",
+                            sender=email,
+                            recipients=[params['gmail_id']],
+                            body = message + "\n" + phone
+                            )
+    return render_template('contact.html', params = params)
 
 app.run(debug=True, port=8001)
